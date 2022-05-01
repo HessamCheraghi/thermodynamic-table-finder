0fb5209d0861479c289f7a18059cf97b70cbc8d6
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-05-01 19:30:53 +0430

    make ui more simple

--- js/view/toggleCalculations.js
+++ js/view/toggleCalculations.js
@@ -1,15 +1,11 @@
 export default function () {
   const checkbox = document.querySelector("#show-calculations");
-  const calculationElement = document.querySelector("#calculations");
-  const resultElement = document.querySelector("#result");
+  const calculationElement = document.querySelector(".tile.is-9.is-parent");
   checkbox.addEventListener("input", () => {
-    if (!checkbox.checked) {
-      calculationElement.style.display = "none";
-      calculationElement.style.visibility = "hidden";
-      resultElement.setAttribute("style", "margin-bottom:0px !important");
+    if (checkbox.checked) {
+      calculationElement.style.display = "block";
     } else {
       calculationElement.removeAttribute("style");
-      resultElement.removeAttribute("style");
     }
   });
 }
